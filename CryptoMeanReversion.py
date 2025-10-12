a187a9b09d2c4eae6722dc1399d8c74c8a1e0769
jaydenavena <jaydenavena4@gmail.com>
2025-10-12 18:07:23 -0500

    abc

--- CryptoMeanReversion.py
+++ CryptoMeanReversion.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,7 +11,7 @@ df = pd.read_csv(csv_path)
 df = df.set_index(pd.DatetimeIndex(df['timestamp'].values))
 df.drop(['timestamp'], inplace = True, axis = 1)
 #drops the first column that was just numbering the rows, makes timestamp the first/organizer one ^
-
+df = df.head(200)
 
 #simple moving average function
 def SMA(data, period = 30, column = 'close'):
@@ -21,9 +22,7 @@ df['SMA'] = SMA(df, 21)
 df['Simple_Returns'] = df.pct_change(1)['close']
 df['Log_Returns'] = np.log(1+df['Simple_Returns'])
 df['Ratios'] = df['close'] / df['SMA']
-
 # print(df['Ratios'].describe())
-# print(df.head(10))
 
 percentiles = [5, 25, 50, 75, 95]
 ratios = df['Ratios'].dropna()
@@ -33,11 +32,12 @@ percentile_values = np.percentile(ratios, percentiles)
 plt.figure(figsize=(12,6))
 plt.title('Ratios')
 df['Ratios'].dropna().plot(legend = True)
-plt.axhline(percentile_values[0], c='green', label = '5th %ile')
-plt.axhline(percentile_values[2], c='yellow', label = '50th %ile')
-plt.axhline(percentile_values[-1], c='red', label = '95th %ile')
-# plt.show()
-# chart kinda funky idk what those spikes are, extreme swings as BTC does i think
+plt.axhline(percentile_values[0], c='green', label = '5th Percentile')
+plt.axhline(percentile_values[2], c='yellow', label = '50th Percentile')
+plt.axhline(percentile_values[-1], c='red', label = '95th Percentile')
+plt.ylabel('Deviation')
+plt.xlabel('Time')
+plt.legend()
 
 buy = percentile_values[0] #buy the 5th percentile
 sell = percentile_values[-1] #sell the 95th
@@ -49,8 +49,13 @@ df['Buy'] = np.where(df.Positions == 1 , df['close'], np.nan)
 df['Sell'] = np.where(df.Positions == -1 , df['close'], np.nan)
 
 plt.figure(figsize = (12,6))
+plt.title("Buy/Sell Signals")
 plt.plot(df['close'], alpha = 0.3, label = 'Close')
 plt.plot(df['SMA'], alpha = 0.5, label = 'SMA')
 plt.scatter(df.index, df['Buy'], color = 'green', label = 'BUY', marker = '^', alpha = 0.8)
 plt.scatter(df.index, df['Sell'], color = 'red', label = 'SELL', marker = 'v', alpha = 0.8)
+plt.xlabel("Time")
+plt.ylabel("Close Price")
+plt.legend()
 plt.show()
+
---
+++ montecarlo.py
@@ -0,0 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy.random import randn
+# crypto monte carlo project maybe
+.gitignore
